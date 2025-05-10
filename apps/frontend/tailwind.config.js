8b2b1ea6160496078a70259961789fe78087eaba
PhoenixZeroph <109314075+PhoenixZeroph@users.noreply.github.com>
2025-05-09 23:36:54 -0300

    Create tailwind.config.js

---
+++ apps/frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#4a0072",
+        accent:  "#b43ef4"
+      }
+    }
+  },
+  plugins: []
+};
