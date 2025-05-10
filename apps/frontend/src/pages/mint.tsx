c0032b53e7e840a6371d2d17b356b45b1052e241
PhoenixZeroph <109314075+PhoenixZeroph@users.noreply.github.com>
2025-05-09 23:23:59 -0300

    Create mint.tsx

---
+++ apps/frontend/src/pages/mint.tsx
@@ -0,0 +1,9 @@
+import WishForm from "@/components/WishForm";
+
+export default function MintPage() {
+  return (
+    <section className="h-screen flex items-center justify-center p-4">
+      <WishForm />
+    </section>
+  );
+}
